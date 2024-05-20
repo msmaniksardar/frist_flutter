7f1328257fb626dc454afa84bbf18e99ff5bf56f
Manik Sardar <anonymousmanik@gmail.com>
2024-05-20 14:19:57 +0600

    start Project

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      theme:
+          ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    ));
+  }
+}
